60a6d1c8df8e28f3739a7c22c2ff03e14d58d43e
Mark Packham <markpackham1@gmail.com>
2019-04-24 18:42:20 +0100

    finish off module

--- modules/custom/drupalup_utils/src/Controller/NameListController.php
+++ modules/custom/drupalup_utils/src/Controller/NameListController.php
@@ -3,6 +3,7 @@
 namespace Drupal\drupalup_utils\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Nubs\RandomNameGenerator\Vgng;
 
 /*
 Our list with weird character names
@@ -12,10 +13,18 @@ class NameListController extends ControllerBase
 
     public function content()
     {
+        $generator = new Vgng();
+
+        $list_of_weird_names = [];
+        for ($i = 1; $i < 10; $i++) {
+            $list_of_weird_names[] = $generator->getName();
+        }
+
         return [
             '#theme' => 'item_list',
             '#list_type' => 'ul',
-            '#items' => ['First', 'Second'],
+            //'#items' => ['Firsty', 'Secondo'],
+            '#items' =>  $list_of_weird_names,
         ];
     }
 
